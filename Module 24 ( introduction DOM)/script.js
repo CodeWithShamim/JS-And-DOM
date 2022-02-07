8e1f4d59c6b9e39dd771e8253798636064349784
codewithshamim <shamimislamonline@gmail.com>
2022-02-07 20:05:35 +0600

    script added

--- Module 24 ( introduction DOM)/script.js
+++ Module 24 ( introduction DOM)/script.js
@@ -1,1 +1,19 @@
-document.write("hello");
+// select element 
+const class1 = document.querySelector('.heading')
+class1.innerHTML = "<h2>Hello</h2>";
+
+// create element 
+const create = document.createElement('p');
+create.innerText = "why are you so angry right now?";
+// add style 
+create.style.fontSize = '30px';
+create.style.color = 'red';
+// add element in parent 
+class1.appendChild(create);
+
+// find parentNode and childNode 
+const result1 = create.parentNode.parentNode;
+console.log(result1);
+
+const result2 = class1.childNodes;
+console.log(result2); // output: nodeList
