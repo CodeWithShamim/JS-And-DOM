787052692b22e1eb97e987ef4b3d12c16d417c11
codewithshamim <shamimislamonline@gmail.com>
2022-02-16 13:15:32 +0600

    string added

---
+++ Module 29(String & Array)/string2.js
@@ -0,0 +1,31 @@
+const text = 'Amar sonar bangla ami tomay valobashi';
+// split------------------------------- 
+// console.log(text.split(""))
+// console.log(text.split(" "))
+// console.log(text.split("r"))
+
+// slice--------------------------------
+// console.log(text.slice(5, 10))
+
+// substr----------------------------------
+// console.log(text.substr(5, 5))
+
+// substring like slice------------------------------------
+// console.log(text.substring(5, 10)) //same to slice
+
+
+// concat--------------------------------
+// const text2 = 'hellow world';
+// const text3 = 100;
+// const text4 = 'welcome';
+// const concating = text2.concat(text3).concat(text4).concat('finished!!');
+// console.log(concating);
+
+// join--------------------------------array item join
+const Array = ['amar', 'sonar', 'b', '24']
+const joining1 = Array.join('')
+const joining2 = Array.join(' ')
+const joining3 = Array.join('? ')
+console.log(joining1);
+console.log(joining2);
+console.log(joining3);
